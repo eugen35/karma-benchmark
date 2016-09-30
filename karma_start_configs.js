651ce2e6d98638bbcc6eee5b337714a45b4a568c
_meugen_ <eugen35@mail.ru>
2016-09-30 19:16:40 +0500

    Оформил (Получилось динамически запускать одну конфигурацию....) в виде функции. Теперь вопрос: как одним махом запускать обе конфигурации? Возможно, бы ещё так, чтобы var shared... не повторять в каждой из конфигураций... Т.е. здесь файлик с загрузкой конфигов, их оверрайдом и запуском их по очереди.

--- karma_start_configs.js
+++ karma_start_configs.js
@@ -2,25 +2,19 @@
 //var bench_config = require('./karma_bench.conf');
 
 var karma = require("karma");
-//var karmaParseConfig = require('karma/lib/config').parseConfig; //Вот утилитка кармы для парсинга конфигурации!!!
 var path = require('path');
-
+//var karmaParseConfig = require('karma/lib/config').parseConfig; //Вот утилитка кармы для парсинга конфигурации!!!
 //var mocha_config = karmaParseConfig( path.resolve('./karma_mocha.conf') , {} ); //Видимо, вместо пустого объекта можно было указать конфиг, накладывающийся на конфиг (уже где-то видел такой пример)
 
-var server = new karma.Server({
-    configFile: path.resolve("./karma_mocha.conf.js")
-}, function(exitCode) {
-    console.log('Karma has exited with ' + exitCode)
-    process.exit(exitCode)
-});
+function runKarmaWithConfig(configFilePath){
+    configFilePath = path.resolve(configFilePath);
 
-server.start();
+    var server = new karma.Server( {configFile: configFilePath}, function(exitCode) {
+        console.log('Karma has exited with ' + exitCode)
+        process.exit(exitCode)
+    });
 
+    server.start();
+}
 
-/*
-var server = new karma.Server({
-    configFile: "../karma_mocha.conf.js"
-}, function(exitCode) {
-    console.log('Karma has exited with ' + exitCode)
-    process.exit(exitCode)
-});*/
+runKarmaWithConfig('./karma_mocha.conf.js');
