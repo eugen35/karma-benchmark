bcfa8416c7d303c05c37ec90b6266c77ff0dff9f
_meugen_ <eugen35@mail.ru>
2016-10-03 09:39:14 +0500

    Тест webdriver с assert-ом теперь запускается... Но нужно попробовать ещё несколько примеров.... Интересно также, что делать с падением вебдрайвера если сравнение в ассерте дёт отрицательный результат...

--- end_to_end/index.js
+++ end_to_end/index.js
@@ -3,7 +3,8 @@
 var By = require('selenium-webdriver').By,
     until = require('selenium-webdriver').until,
     chrome = require('selenium-webdriver/chrome'),
-    test = require('selenium-webdriver/testing');
+    test = require('selenium-webdriver/testing'),
+    assert = require('selenium-webdriver/testing/assert');
 
 
 test.describe('Google Search', function() {
@@ -18,11 +19,23 @@ test.describe('Google Search', function() {
     driver.quit();
   });
 
-
+/*
   test.it('should append query to title', function() {
     driver.get('http://www.google.com/ncr');
     driver.findElement(By.name('q')).sendKeys('webdriver');
     driver.findElement(By.name('btnG')).click();
     driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+  }); */
+
+  test.it('should append query to title', function() {
+
+    //assert(2).equalTo(2,'Равно');
+    driver.get('file:///D:/projects/karma-benchmark/index.html');
+    assert(driver.getTitle()).equalTo('My Project','Равно');
+    /*
+    driver.findElement(By.name('q')).sendKeys('webdriver');
+    driver.findElement(By.name('btnG')).click();
+    driver.wait(until.titleIs('webdriver - Google Search'), 1000);*/
   });
+
 });
