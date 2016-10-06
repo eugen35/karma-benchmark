70ad2e18283c9b075a0b42b7e0632fc88de50875
_meugen_ <eugen35@mail.ru>
2016-10-06 09:47:18 +0500

    Теперь нужно end_to_end тест chrome переписан в вариант chai-as-promised

--- end_to_end/chrome.js
+++ end_to_end/chrome.js
@@ -1,43 +1,56 @@
 //запускать нужно так mocha -t 10000 end_to_end/index.js - таймаут нужен побольше, ведь браузер открывается и всё такое
 //js api webdriver(см. меню справа) http://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/index.html
 
-var By = require('selenium-webdriver').By,
-    until = require('selenium-webdriver').until,
-    chrome = require('selenium-webdriver/chrome'), //драйвер firefox что-то глючит (многие пишут), причём не гуглится толком как избавиться от ошибки.
-    test = require('selenium-webdriver/testing'),
-    assert = require('selenium-webdriver/testing/assert');
+
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+var expect = chai.expect
+
+var By = require('selenium-webdriver').By;
+
+//Выбираем браузер (можно безголовый выбрать)
+var selenium = require('selenium-webdriver');
+/*
+var phantomjs_exe = require('phantomjs-prebuilt').path; //phantomjs-prebuilt - это пакет npmjs, ему не нужен бинарный драйвер фантома, засунутый в корень папки проекта
+var browser = selenium.Capabilities.phantomjs(); //В официальном примере данная переменная называлась не browser, а customPhantom
+browser.set("phantomjs.binary.path", phantomjs_exe);
+*/
+var browser = selenium.Capabilities.chrome(); //тут пакетов npmjs не нужно, но нужно бинарный драйвер chrome класть в только в корень папки проекта
+
+var driver = new selenium.Builder().
+withCapabilities(browser).
+build();
 
 
-test.describe('Chrome: 1 Проверяем index.html', function() {
-  var driver;
 
-  test.before(function() {
-    driver = new chrome.Driver();
+
+  describe('1.1 Должно совпадать название и часть текста страницы', function() {
+    after(function(){
+      driver.quit();
+    });
+
+    it('1.1 Должно совпадать название страницы', function() {
+      driver.get('file:///D:/projects/karma-benchmark/index.html');
+      return expect(driver.getTitle()).to.eventually.equal('My Project');
+      //а вот повторное выполнение сравнения работает, видимо, здесь нужно использовать Promise.all() или что-то в этом роде
+      //return expect(driver.findElement(By.css('body')).getText()).to.eventually.include('Hello1');
+    });
+
+
+    it('1.2 Должна совпадать часть текста страницы', function() {
+      //driver.get('file:///D:/projects/karma-benchmark/index.html');
+      return expect(driver.findElement(By.css('body')).getText()).to.eventually.include('Hello');
+    });
+
   });
 
-  test.after(function() {
-    driver.quit();
-  });
 
-  test.it('1.1 Должно совпадать название и часть текста страницы', function() {
-    //assert(2).equalTo(2,'Равно');
-    driver.get('file:///D:/projects/karma-benchmark/index.html');
-    assert(driver.getTitle()).equalTo('My Project', 'Название страницы не совпадает');
-    assert(driver.findElement(By.css('body')).getText()).contains('Hello1','Текст Hello не встречается внутри элемента body');
-  });
-
-  test.it('1.2 Должна совпадать часть текста страницы', function() {
-    driver.get('file:///D:/projects/karma-benchmark/index.html');
-    assert(driver.findElement(By.css('body')).getText()).contains('Hello','Текст Hello не встречается внутри элемента body');
-  });
-
-   /*
-   test.it('should append query to title', function() {
-   driver.get('http://www.google.com/ncr');
-   driver.findElement(By.name('q')).sendKeys('webdriver');
-   driver.findElement(By.name('btnG')).click();
-   driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-   });
-   */
-
-});
+/*
+ test.it('should append query to title', function() {
+ driver.get('http://www.google.com/ncr');
+ driver.findElement(By.name('q')).sendKeys('webdriver');
+ driver.findElement(By.name('btnG')).click();
+ driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+ });
+ */
